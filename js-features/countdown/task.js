568353ae6e09e43a3ca927004e5b8f656834271e
Tryakin Mikhail <mixazbubl@yandex.ru>
2020-12-28 20:15:07 +0300

    ver 1.1
    task 1

---
+++ js-features/countdown/task.js
@@ -0,0 +1,15 @@
+const count = document.getElementById('timer')
+let i = +count.textContent
+console.log(count.textContent)
+const newCount = setInterval(() => {
+  if (i ===0) {
+    location.assign("https://lh3.googleusercontent.com/proxy/DkT149dZ-8ZIxz-1wSem3x9nI32Zy46uOERnAUok_rdCdPIKjO03YhcOvcNsh5sl8NzUrj0O4x8Kx_j3mg539SNO8VY");
+    alert('Вы победили в конкурсе')
+    clearInterval(newCount)
+  }
+  if (i < 10) {
+    count.textContent = `00:00:0${i--}`
+  } else {
+    count.textContent = `00:00:${i--}`
+  }
+}, 1000);
