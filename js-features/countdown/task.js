0e6e0afd5d01737b2dd7e52703f1914d17cfd522
Tryakin Mikhail <mixazbubl@yandex.ru>
2020-12-28 20:18:21 +0300

    ver 1.1.1
    edit code

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,8 +1,7 @@
 const count = document.getElementById('timer')
 let i = +count.textContent
-console.log(count.textContent)
 const newCount = setInterval(() => {
-  if (i ===0) {
+  if (i === 0) {
     location.assign("https://lh3.googleusercontent.com/proxy/DkT149dZ-8ZIxz-1wSem3x9nI32Zy46uOERnAUok_rdCdPIKjO03YhcOvcNsh5sl8NzUrj0O4x8Kx_j3mg539SNO8VY");
     alert('Вы победили в конкурсе')
     clearInterval(newCount)
