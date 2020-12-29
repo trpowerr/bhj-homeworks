7fadf05c2f7795caa779ed2ce8870527890f993f
Tryakin Mikhail <mixazbubl@yandex.ru>
2020-12-29 23:21:28 +0300

    ver 1.2.1
    task 1

---
+++ element-search/popups/task.js
@@ -0,0 +1,26 @@
+const newModal = document.getElementById('modal_main')
+newModal.classList.add('modal_active')
+
+const success = document.getElementsByClassName('show-success')
+const newModalSuccess = document.getElementById('modal_success')
+success.item(0).onclick = () => {
+  newModal.classList.remove('modal_active')
+  newModalSuccess.classList.add('modal_active')
+}
+
+const close = document.querySelectorAll('div.modal__close_times')
+function f ([...param]){
+  for (let value in param) {
+    param[value].onclick = () =>{
+      newModal.classList.remove('modal_active')
+      newModalSuccess.classList.remove('modal_active')
+    }
+  }
+}
+f(close)
+
+const btnGoodJob = document.querySelector('a.btn_success')
+btnGoodJob.onclick = () =>{
+  newModalSuccess.classList.remove('modal_active')
+}
+
