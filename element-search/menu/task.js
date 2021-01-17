e4a7e0eb272e61491c6fb608cd968e8b5a5f68de
Tryakin Mikhail <mixazbubl@yandex.ru>
2021-01-17 21:49:27 +0300

    bugfix

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,15 +1,17 @@
 const itemMenuLink = document.querySelectorAll('a.menu__link')
 
 for (i = 0; i < 9; i++) {
-  itemMenuLink.item(i).onclick = () => {
+  itemMenuLink.item(i).onclick = function() {
     const itemMenuSubActive = document.querySelector('ul.menu_active')
+    const closeItem = itemMenuSub.classList.contains('menu_active')
+    
     if (itemMenuSubActive !== null) {
       itemMenuSubActive.classList.remove('menu_active')
     }
-    if (itemMenuSub !== null) {
+    if (closeItem === false && itemMenuSub !== null) {
       itemMenuSub.classList.add('menu_active')
-      return false
     }
+    return false
   }
   const itemMenuSub = itemMenuLink.item(i).closest('li.menu__item').querySelector('ul.menu.menu_sub')
 }
--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -3,24 +3,24 @@ newModal.classList.add('modal_active')
 
 const success = document.getElementsByClassName('show-success')
 const newModalSuccess = document.getElementById('modal_success')
-success.item(0).onclick = () => {
+success.item(0).onclick = function() {
   newModal.classList.remove('modal_active')
   newModalSuccess.classList.add('modal_active')
 }
 
 const close = document.querySelectorAll('div.modal__close_times')
+const modal = document.querySelector('.modal__content')
 function f ([...param]){
   for (let value in param) {
-    param[value].onclick = () =>{
-      newModal.classList.remove('modal_active')
-      newModalSuccess.classList.remove('modal_active')
+    param[value].onclick = function() {
+      modal.closest('.modal').classList.remove('modal_active')
     }
   }
 }
 f(close)
 
 const btnGoodJob = document.querySelector('a.btn_success')
-btnGoodJob.onclick = () =>{
+btnGoodJob.onclick = function() {
   newModalSuccess.classList.remove('modal_active')
 }
 
