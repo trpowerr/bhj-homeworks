0be3bde1ad8cb4b029c150041e9371c013108b96
Tryakin Mikhail <mixazbubl@yandex.ru>
2021-01-25 22:14:36 +0300

    2.2.3
    task 3

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -67,26 +67,17 @@ class Autocomplete {
     this.list.innerHTML = html.join('');
   }
 
-  getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+  getMatches( text ) {  
+    let arr = [];
+      for (let i = 0; i < this.input.length; i++) {
+        if (this.input[i].textContent.includes(text)) {
+          arr.push({
+            text: this.input[i].textContent,
+            value: this.input.value
+          });
+        }
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    return arr
   }
 }
 
