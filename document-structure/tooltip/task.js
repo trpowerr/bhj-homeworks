33ff866ddc65935ea9aa3bbec9334e0e0d27234e
Tryakin Mikhail <mixazbubl@yandex.ru>
2021-02-02 11:27:58 +0300

    2.3.1 bugfix

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,15 +1,24 @@
 const textClick = document.querySelectorAll('.has-tooltip');
 const block = document.querySelector('.tooltip');
+let prevTitle = null;
 
 function clickFunc(e) {
 	e.preventDefault();
 	const title = e.target.getAttribute('title');
-	const dY = e.target.getBoundingClientRect().top;
-	const dX = e.target.getBoundingClientRect().left;
-	block.textContent = title;
-	block.style.display = 'block';
-	block.style.top = `${dY + 20}px`;
-	block.style.left = `${dX}px`;
+	console.log(title, prevTitle)
+	if (prevTitle !== title) {
+		prevTitle = title
+		const dY = e.target.getBoundingClientRect().top;
+		const dX = e.target.getBoundingClientRect().left;
+		block.textContent = title;
+		block.style.display = 'block';
+		block.style.top = `${dY + 20}px`;
+		block.style.left = `${dX}px`;
+	} else {
+		prevTitle = null
+		block.style.display = 'none';
+	}
+	
 }
 
 for(let text of textClick) {
