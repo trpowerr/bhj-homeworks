7ed5f88382ed2e6c2385298fb874112ca626da53
Tryakin Mikhail <mixazbubl@yandex.ru>
2021-02-03 16:38:50 +0300

    bugfix

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,15 +1,15 @@
-const progress = document.getElementById('progress');//получаем полосу прогресса
-let form = document.getElementById('form'); //получаем элемент формы
-const button = document.getElementById('send'); //получаем кнопку
-let formData = new FormData(form);//создаём форму для отправки
-let xhr = new XMLHttpRequest();//создаём запрос
-button.addEventListener('click', sendForm); //обработчик на кнопку
-function sendForm() {//отправляем запрос
+const progress = document.getElementById('progress');
+let form = document.getElementById('form');
+const button = document.getElementById('send');
+let formData = new FormData(form);
+let xhr = new XMLHttpRequest();
+button.addEventListener('click', sendForm); 
+function sendForm() {
     event.preventDefault();
     xhr.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
-    xhr.send(formData); //отправляем форму
+    xhr.send(formData); 
     let file = form.getElementsByTagName('input')[0];
-    let fileSize = file.files[0].size; //перевожу байты в биты
+    let fileSize = file.files[0].size;
     xhr.addEventListener('progress', function(event) {
         let kyloBytesLoaded = event.loaded;
         let percents = kyloBytesLoaded / fileSize;
